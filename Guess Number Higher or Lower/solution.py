4773f10d6f37e5148a09aa84748bcd912ec6000e
MayuriSulane <160130027+MayuriSulane@users.noreply.github.com>
2024-05-20 07:09:23 +0000

    smaller or higher number

---
+++ Guess Number Higher or Lower/solution.py
@@ -0,0 +1,33 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+      
+        low = 0
+        high = n
+        mid = 0
+    
+        while low <= high:
+    
+            mid = (high + low) // 2
+    
+            
+            if guess(mid)== 1:
+                low = mid + 1
+    
+           
+            elif guess(mid) == -1:
+                high = mid - 1
+    
+            else:
+                return mid
+        
