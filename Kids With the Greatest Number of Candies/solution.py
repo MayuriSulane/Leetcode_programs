31531eeae78303db596f29af7ef62e58a99ef1d6
MayuriSulane <160130027+MayuriSulane@users.noreply.github.com>
2024-05-20 08:46:02 +0000

    maximum candies

---
+++ Kids With the Greatest Number of Candies/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        # res = []
+        # for i in candies:
+        #     if (i + extraCandies) >= max(candies):
+        #         res.append(True)
+        #     else:
+        #         res.append(False)
+        # return res
+        return [(i + extraCandies) >= max(candies) for i in candies]
